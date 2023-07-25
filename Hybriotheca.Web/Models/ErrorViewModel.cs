163f6c8726f442703fd844c2e0a05c82cb4362e7
ShinyNchrome <Pedro.Silva.28348@formandos.cinel.pt>
2023-07-25 14:22:45 +0100

    Init project

---
+++ Hybriotheca.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hybriotheca.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
