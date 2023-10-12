ee977d4741613ba1610b2506e67464d4453a2986
ShinyNchrome <Pedro.Silva.28348@formandos.cinel.pt>
2023-10-12 17:28:07 +0100

    Update IBookEditionRepository.cs
    Remove duplicate method UpdateKeepCoverImageAsync()
    Order methods alphabetically

--- Hybriotheca.Web/Repositories/Interfaces/IBookEditionRepository.cs
+++ Hybriotheca.Web/Repositories/Interfaces/IBookEditionRepository.cs
@@ -6,15 +6,9 @@ namespace Hybriotheca.Web.Repositories.Interfaces;
 
 public interface IBookEditionRepository : IGenericRepository<BookEdition>
 {
-    Task UpdateKeepCoverImageAsync(BookEdition bookEdition);
-
-    Task<IEnumerable<SelectListItem>> GetComboBookEditionsAsync();
-
-    Task<CarouselEditionsViewModel> GetCarouselEditionsAsync();
-
     Task<List<BookEdition>> CarouselEditionsInfiniteScrollAsync(string category, int lastEditionID);
 
-    Task<SearchViewModel> GetSearchResultsAsync(SearchViewModel viewModel);
+    Task<CarouselEditionsViewModel> GetCarouselEditionsAsync();
 
     IEnumerable<string> GetCheckBoxCategories();
 
@@ -23,6 +17,12 @@ public interface IBookEditionRepository : IGenericRepository<BookEdition>
     IEnumerable<string> GetCheckBoxLang();
 
     IEnumerable<string> GetCheckBoxPubYear();
+
+    Task<IEnumerable<SelectListItem>> GetComboBookEditionsAsync();
+
+    Task<SearchViewModel> GetSearchResultsAsync(SearchViewModel viewModel);
+
     Task<bool> IsConstrainedAsync(int id);
+
     Task UpdateKeepCoverImageAsync(BookEdition bookEdition);
 }
