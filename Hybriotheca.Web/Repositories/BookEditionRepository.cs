61366ce8cbfa8b01db32870141141b4d65178169
Dario Dias <dariodias97@gmail.com>
2023-10-19 06:51:20 +0100

    Revert "Add TempData message to Login View"
    This reverts commit 4a2facd0dbadb605d23f0555a97038387f0aa668.

--- Hybriotheca.Web/Repositories/BookEditionRepository.cs
+++ Hybriotheca.Web/Repositories/BookEditionRepository.cs
@@ -84,18 +84,11 @@ public class BookEditionRepository : GenericRepository<BookEdition>, IBookEditio
         int currentPage = viewModel.Page;
 
         string searchUrl = "/Search?";
-        var editionsQuery = _dataContext.BookEditions
-            .AsNoTracking()
-            .Include(a => a.Book)
-            .Include(s => s.Ratings)
-            .AsSplitQuery();
+        var editionsQuery = _dataContext.BookEditions.Include(a => a.Book).AsSplitQuery().Include(s => s.Ratings).AsSplitQuery().AsQueryable();
 
         if (viewModel.Categories is not null && viewModel.Categories.Any())
         {
-            editionsQuery = editionsQuery
-                .Include(s => s.Category)
-                .AsSplitQuery()
-                .Where(x => EF.Functions.Contains(viewModel.Categories, x.Category.Name));
+            editionsQuery = editionsQuery.Include(s => s.Category).AsSplitQuery().Where(x => viewModel.Categories.Contains(x.Category.Name));
 
             for (int i = 0; i < viewModel.Categories.Count; i++)
             {
@@ -213,11 +206,11 @@ public class BookEditionRepository : GenericRepository<BookEdition>, IBookEditio
 
         if (!string.IsNullOrWhiteSpace(viewModel.SearchTerm))
         {
-            editionsQuery = editionsQuery.Where(s =>
-                EF.Functions.Contains(s.EditionTitle, viewModel.SearchTerm) ||
-                EF.Functions.Contains(s.Book.Author, viewModel.SearchTerm) ||
-                (s.ISBN != null && EF.Functions.Contains(s.ISBN, viewModel.SearchTerm)) ||
-                EF.Functions.Contains(s.Publisher, viewModel.SearchTerm));
+            editionsQuery = editionsQuery.Where(
+            s => s.EditionTitle.Contains(viewModel.SearchTerm) ||
+            s.Book.Author.Contains(viewModel.SearchTerm) ||
+            s.ISBN.Contains(viewModel.SearchTerm) ||
+            s.Publisher.Contains(viewModel.SearchTerm));
 
             if (!_hasExistingParams)
             {
